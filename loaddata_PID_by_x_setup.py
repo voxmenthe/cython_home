0885612a5ff5c30956b797a69d29ca912e51007f
voxmenthe <jeffcogg@gmail.com>
2019-05-26 22:54:04 -0700

    auto commit from home May262019_225403

--- loaddata_PID_by_x_setup.py
+++ loaddata_PID_by_x_setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 setup(ext_modules=cythonize("loaddata_PID_by_x.pyx"))
 
-# complilation: python loaddata_PID__by_x_setup.py build_ext --inplace
+# complilation: python loaddata_PID_by_x_setup.py build_ext --inplace

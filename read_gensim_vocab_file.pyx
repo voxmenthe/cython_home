0852f992e9303f6076e337ae7c152dc94ffe0504
Jeff Coggshall <jeff.coggshall@macys.com>
2019-06-04 09:51:52 -0700

    automated commit from laptop Tue Jun  4 09:51:51 PDT 2019

---
+++ read_gensim_vocab_file.pyx
@@ -0,0 +1,16 @@
+# not working yet due to not adding pandas stuff
+import csv
+
+def read_gensim_vocab_file(vocfile,veclen=128):
+	cdef list row
+	cdef int i
+	with open(vocfile,'r') as f:
+		rows = []
+		csv_reader = csv.reader(f,delimiter="\t")
+		for i, row in enumerate(csv_reader):
+			row = row[0].strip().split()
+			rows.append(row[:-1])
+		df = pd.DataFrame(rows).set_index(0).fillna(value='ZZ99ZZ99').reset_index().astype('str')
+		#df - apply remove '' before summing
+		#df = df.apply(lambda x: x.str.replace("'","") if x.dtype == "object" else x) # not doing this b/c hard to strip from sub_attr
+	return list(df.sum(axis=1).astype('str').str.replace('ZZ99ZZ99',""))

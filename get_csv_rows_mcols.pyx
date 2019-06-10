6a52e43aee14cfeedf822936d0ee024902d8a927
Jeff Coggshall <jeff.coggshall@macys.com>
2019-06-10 15:27:42 -0700

    automated commit from laptop Mon Jun 10 15:27:41 PDT 2019

--- get_csv_rows_mcols.pyx
+++ get_csv_rows_mcols.pyx
@@ -1,16 +1,22 @@
 import csv
 
-def get_csv_rows_mcols(path, numrows=None,numcols=[]):
+def get_csv_rows_mcols(path, numrows=None,numcols=[],truncate=True):
     cdef dict data
     cdef list row
     all_rows = []
+    
+    if numcols:
+        mincols = min(numcols)
 
     with open(path, "r") as data_file:
         csv_reader = csv.reader(data_file, delimiter="\t")
         for row in csv_reader:
             if numcols:
                 if len(row) in numcols:
-                    all_rows.append(row[:min(numcols)])
+                    if truncate:
+                        all_rows.append(row[:mincols])
+                    else:
+                        all_rows.append(row)
             else:
                 all_rows.append(row)
             if numrows and len(all_rows) >= numrows:

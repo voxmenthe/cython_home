4f54a15625ded930017dd775a54b36111d958c3c
voxmenthe <jeffcogg@gmail.com>
2019-06-23 12:37:50 -0700

    auto commit from home Jun232019_123750

--- loaddata_SUB.pyx
+++ loaddata_SUB.pyx
@@ -1,15 +1,15 @@
 def loaddata_SUB(df,mapping_dict):
-	data = OrderedDict()
-	
-	for row in df.iterrows():
-		pid = row[1][1]
-		visitor_id = str(row[1][3])
+    data = OrderedDict()
+    
+    for row in df.iterrows():
+        pid = row[1][1]
+        visitor_id = str(row[1][3])
 
-		if visitor_id not in data:
-			data[visitor_id] = []
+        if visitor_id not in data:
+            data[visitor_id] = []
 
-		try:
-			data[visitor_id].append(mapping_dict[pid])
-		except KeyError:
-			 continue
-	return data
+        try:
+            data[visitor_id].append(mapping_dict[pid])
+        except KeyError:
+             continue
+    return data

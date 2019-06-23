25b196fd9e1cf05631ea2c41ad30b94b6a4c4f6d
voxmenthe <jeffcogg@gmail.com>
2019-06-23 12:41:18 -0700

    auto commit from home Jun232019_124118

--- read_gensim_vocab_file.pyx
+++ read_gensim_vocab_file.pyx
@@ -10,7 +10,7 @@ def read_gensim_vocab_file(vocfile,veclen=128):
 		for i, row in enumerate(csv_reader):
 			row = row[0].strip().split()
 			rows.append(row[:-1])
-		df = pd.DataFrame(rows).set_index(0).fillna(value='ZZ99ZZ99').reset_index().astype('str')
+		#df = pd.DataFrame(rows).set_index(0).fillna(value='ZZ99ZZ99').reset_index().astype('str')
 		#df - apply remove '' before summing
 		#df = df.apply(lambda x: x.str.replace("'","") if x.dtype == "object" else x) # not doing this b/c hard to strip from sub_attr
-	return list(df.sum(axis=1).astype('str').str.replace('ZZ99ZZ99',""))
+	return rows #list(df.sum(axis=1).astype('str').str.replace('ZZ99ZZ99',""))

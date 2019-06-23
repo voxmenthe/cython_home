ab0419f5a5bef84ac6e314006561c5d14180e0d8
voxmenthe <jeffcogg@gmail.com>
2019-06-23 10:51:05 -0700

    auto commit from home Jun232019_105105

--- get_csv_rows.pyx
+++ get_csv_rows.pyx
@@ -1,18 +1,27 @@
 import csv
 
-def get_csv_rows(path, numrows=None,numcols=None):
+def get_csv_rows(path, numrows=None,numcols=None,truncate=False):
     cdef dict data
     cdef list row
+    cdef int counter
     all_rows = []
 
     with open(path, "r") as data_file:
         csv_reader = csv.reader(data_file, delimiter="\t")
+        if numrows: counter = 0
+        assert not truncate and numcols "Can only set one of truncate or numcols but not both"
         for row in csv_reader:
-            if numcols:
+
+            if numrows:
+                counter + =1
+                if counter >= numrows: break
+
+            if truncate:
+                row = all_rows.append(row[:truncate])
+            elif numcols:
                 if len(row) == numcols:
                     all_rows.append(row)
             else:
                 all_rows.append(row)
-            if numrows and len(all_rows) >= numrows:
-                break
+                
     return all_rows

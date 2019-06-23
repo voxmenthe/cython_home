a58a8d0eabe39a3e0a0c30a946b2f298936749d9
voxmenthe <jeffcogg@gmail.com>
2019-06-23 10:53:22 -0700

    auto commit from home Jun232019_105322

--- get_csv_rows.pyx
+++ get_csv_rows.pyx
@@ -9,7 +9,7 @@ def get_csv_rows(path, numrows=None,numcols=None,truncate=False):
     with open(path, "r") as data_file:
         csv_reader = csv.reader(data_file, delimiter="\t")
         if numrows: counter = 0
-        assert not truncate and numcols "Can only set one of truncate or numcols but not both"
+        assert not truncate and numcols, "Can only set one of truncate or numcols but not both"
         for row in csv_reader:
 
             if numrows:
@@ -23,5 +23,5 @@ def get_csv_rows(path, numrows=None,numcols=None,truncate=False):
                     all_rows.append(row)
             else:
                 all_rows.append(row)
-                
+
     return all_rows

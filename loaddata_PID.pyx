cd0d1ec4455ddf3b236e12310a643df1ea795d9e
voxmenthe <jeffcogg@gmail.com>
2019-06-23 12:24:11 -0700

    auto commit from home Jun232019_122410

--- loaddata_PID.pyx
+++ loaddata_PID.pyx
@@ -1,21 +1,21 @@
 import csv
 
 def loaddata_PID(data,path,numrows=None):
-    #data = dict()
-    cdef dict data = {}
+	#data = dict()
+	cdef dict data = {}
 
-    with open(path, "r") as view_file:
-        csv_reader = csv.reader(view_file, delimiter="\t")
-        for id, row in enumerate(csv_reader):
-            product = str(row[1])
-            visitor_id = row[3]
+	with open(path, "r") as view_file:
+		csv_reader = csv.reader(view_file, delimiter="\t")
+		for id, row in enumerate(csv_reader):
+			product = str(row[1])
+			visitor_id = row[3]
 
-            if visitor_id not in data:
-                data[visitor_id] = []
-            try:
-                data[str(visitor_id)].append(product)
-            except KeyError:
-                 continue
-            if numrows and len(data) >= numrows:
-                break
-    return data
+			if visitor_id not in data:
+				data[visitor_id] = []
+			try:
+				data[str(visitor_id)].append(product)
+			except KeyError:
+				 continue
+			if numrows and len(data) >= numrows:
+				break
+	return data

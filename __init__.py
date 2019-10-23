04a922de34e7d323790ecd88ffbd5ecf65ec55cd
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-23 16:03:52 -0700

    automated commit from laptop Wed Oct 23 16:03:46 PDT 2019

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
-__all__ = ["create_precs", "create_ur_recs", "create_ur_recs_dict_and_csv_full_row", "get_attr_rows", "get_csv_rows", "get_csv_rows_v2", "load_P_recs", "loaddata_PID", "loaddata_PID_by_x", "threshold", "loaddata_SUB_from_rows"]
+__all__ = [
+			"create_precs", "create_ur_recs", "create_ur_recs_dict_and_csv_full_row", 
+			"get_attr_rows", "get_csv_rows", "get_csv_rows_v2", "load_P_recs", 
+			"loaddata_PID", "loaddata_PID_by_x", "threshold", "loaddata_SUB_from_rows",
+			"csv_event_rows_to_dict"]

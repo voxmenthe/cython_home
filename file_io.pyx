159725a668a8a6a162f21b513627d7966d62307f
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-28 15:03:47 -0700

    automated commit from laptop Mon Oct 28 15:03:40 PDT 2019

--- file_io.pyx
+++ file_io.pyx
@@ -2,9 +2,9 @@ from libc.stdio cimport *
 from libc.stdlib cimport malloc
 from libc.string cimport strcpy, strlen
 
-import numpy as np
-cimport numpy as np
- 
+#import numpy as np
+#cimport numpy as np
+
 cdef extern from "stdio.h":
     #FILE * fopen ( const char * filename, const char * mode )
     FILE *fopen(const char *, const char *)
@@ -102,6 +102,7 @@ def read_file3(filename):
  
     cdef FILE* cfile
     
+    cdef str row
     cdef list output
     output = []
 
@@ -116,44 +117,10 @@ def read_file3(filename):
     while True:
         read = getline(&line, &l, cfile)
         if read == -1: break
-        output.append(np.array(line.split(b'\t'),dtype='<U64'))
+        row = line.decode("UTF-8")
+        output.append(row.split('\t'))
         #yield line
  
     fclose(cfile)
  
     return output #[]
-
-# def read_file4(filename):
-#     filename_byte_string = filename.encode("UTF-8")
-#     cdef char* fname = filename_byte_string
- 
-#     cdef FILE* cfile
-    
-#     cdef list output
-#     output = []
-
-#     cfile = fopen(fname, "rb")
-#     if cfile == NULL:
-#         raise FileNotFoundError(2, "No such file or directory: '%s'" % filename)
-
-#     #cdef char * line = NULL
-#     cdef char * l = c_call_returning_a_c_string()
-#     #cdef size_t l = 0
-#     #cdef Py_ssize_t l = 0
-#     cdef ssize_t read
-    
-#     while True:
-#         read = getline(&line, &l, cfile)
-#         if read == -1: break
-        
-#         # get pointer and length from a C function
-#         #get_a_c_string(&line, &l)
-
-#         #uline = line[:l].decode('UTF-8')
-#         uline = line.decode('UTF-8')
-#         output.append(uline.split('\t'))
-#         #yield line
- 
-#     fclose(cfile)
- 
-#     return output #[]

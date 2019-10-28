7f6c1ea8585025bd09515cedc2c9d2f41ca2a87e
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-28 15:13:44 -0700

    automated commit from laptop Mon Oct 28 15:13:39 PDT 2019

--- file_io.pyx
+++ file_io.pyx
@@ -125,13 +125,12 @@ def read_file3(filename):
  
     return output #[]
 
-def read_file_to_dict(filename):
+def read_file3a(filename):
     filename_byte_string = filename.encode("UTF-8")
     cdef char* fname = filename_byte_string
  
     cdef FILE* cfile
     
-    cdef str row
     cdef list output
     output = []
 
@@ -146,7 +145,37 @@ def read_file_to_dict(filename):
     while True:
         read = getline(&line, &l, cfile)
         if read == -1: break
-        row = line.decode("UTF-8").split('\t')
+        output.append(line.decode("UTF-8").split('\t'))
+        #yield line
+ 
+    fclose(cfile)
+ 
+    return output #[]
+
+def read_file_to_dict(filename):
+    filename_byte_string = filename.encode("UTF-8")
+    cdef char* fname = filename_byte_string
+ 
+    cdef FILE* cfile
+    
+    cdef str decoded
+    cdef list row
+    cdef list output
+    output = []
+
+    cfile = fopen(fname, "rb")
+    if cfile == NULL:
+        raise FileNotFoundError(2, "No such file or directory: '%s'" % filename)
+
+    cdef char * line = NULL
+    cdef size_t l = 0
+    cdef ssize_t read
+ 
+    while True:
+        read = getline(&line, &l, cfile)
+        if read == -1: break
+        decoded = line.decode("UTF-8")
+        row = decoded.split('\t')
         output.append((row[0], (row[1], row[2])))
         #yield line
  

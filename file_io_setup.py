65199a1244c5a527380f0971068567ca7bfa3d03
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-28 13:19:26 -0700

    automated commit from laptop Mon Oct 28 13:19:21 PDT 2019

--- file_io_setup.py
+++ file_io_setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize("file_io.pyx"))
-
+setup(ext_modules=cythonize("file_io.pyx", include_path=[numpy.get_include()]))
 # complilation: python file_io_setup.py build_ext --inplace

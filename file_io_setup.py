32b4a0f2e90746f7b28361b1ba9444931fae716c
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-27 18:06:09 -0700

    automated commit from laptop Sun Oct 27 18:06:02 PDT 2019

---
+++ file_io_setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+setup(ext_modules=cythonize("file_io.pyx"))
+
+# complilation: python file_io_setup.py build_ext --inplace
--- process_chunk.pyx
+++ process_chunk.pyx
@@ -2,7 +2,6 @@ def process_chunk(csv_generator, data_dict):
 	cdef list row
 	for chunk in csv_generator:
 		for row in chunk:
-			print("row": row)
 			if row[0] in data_dict.keys():
 				data_dict[row[0]].append((row[1],row[2]))
 			else:

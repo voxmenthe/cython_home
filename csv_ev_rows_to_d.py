234186bb3a09af0c882cc1188fab755293549b46
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-30 15:06:59 -0700

    automated commit from laptop Wed Oct 30 15:06:46 PDT 2019

--- csv_ev_rows_to_d.py
+++ csv_ev_rows_to_d.py
@@ -11,14 +11,20 @@ cdef extern from "stdio.h":
     #ssize_t getline(char **lineptr, size_t *n, FILE *stream);
     ssize_t getline(char **, size_t *, FILE *)
 
-def read_file_ev_rows(filename):
+def read_file_ev_rows(filename, chunksize=600000):
     filename_byte_string = filename.encode("UTF-8")
     cdef char* fname = filename_byte_string
  
     cdef FILE* cfile
     
+    cdef list row
     cdef list output
+    cdef dict data
+    cdef int counter
+
     output = []
+    data = {}
+    counter = 0
 
     cfile = fopen(fname, "rb")
     if cfile == NULL:
@@ -29,9 +35,11 @@ def read_file_ev_rows(filename):
     cdef ssize_t read
  
     while True:
+        counter += 1
         read = getline(&line, &l, cfile)
         if read == -1: break
-        output.append(line.split(b'\t'))
+        row = line.split(b'\t')
+        output.append((row[0],(row[1],row[2])))
         #yield line
  
     fclose(cfile)

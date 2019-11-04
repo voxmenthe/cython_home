3ac0033acd8d0cdf10eab2997964134a0fc979b8
Jeff Coggshall <jeff.coggshall@macys.com>
2019-11-04 13:30:43 -0800

    automated commit from laptop Mon Nov  4 13:30:37 PST 2019

--- load_attr_rows_basic.pyx
+++ load_attr_rows_basic.pyx
@@ -5,8 +5,16 @@ cdef extern from "stdio.h":
     int fclose(FILE *)
     ssize_t getline(char **, size_t *, FILE *)
 
+"""
+Assuming these column positions are still valid:
+[('product_id', 0), ('prod_name', 1), ('today_price', 2), ('availability_flag', 5), ('home_category_id', 6), 
+ ('gmm_id', 7), ('div_id', 8), ('department_id', 9), ('prod_age_txt', 10), ('new_arrival', 11), ('brand', 12), 
+ ('age_category', 15), ('gender', 17), ('master_product_flag', 18), ('recommend_pdp', 19), ('site_product_type', 21), 
+ ('pwp_ind', 24)]
+ """
 
-def load_pid_ptype_dict_from_attr(filename):
+
+def load_attr_rows_basic(filename):
     filename_byte_string = filename.encode("UTF-8")
     cdef char* fname = filename_byte_string
  
@@ -14,8 +22,8 @@ def load_pid_ptype_dict_from_attr(filename):
     
     cdef str decoded
     cdef list row
-    cdef dict output_dict
-    output_dict = {}
+    cdef list output_rows
+    output_rows = []
 
     cfile = fopen(fname, "rb")
     if cfile == NULL:
@@ -30,8 +38,8 @@ def load_pid_ptype_dict_from_attr(filename):
         if read == -1: break
         decoded = line.decode("UTF-8")
         row = decoded.split('\t')
-        output_dict[row[0]] = row[21]
+        output_rows.append( [ row[0:3], row[5:13], row[15], row[17:20], row[21], row[24] ] )
  
     fclose(cfile)
  
-    return output_dict
+    return output_rows

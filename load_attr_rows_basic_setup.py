e57d87aeac559b51b71659da31841e9529f54922
Jeff Coggshall <jeff.coggshall@macys.com>
2019-11-04 13:26:50 -0800

    automated commit from laptop Mon Nov  4 13:26:43 PST 2019

--- load_attr_rows_basic_setup.py
+++ load_attr_rows_basic_setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize("load_attr_rows_new.pyx"))
+setup(ext_modules=cythonize("load_attr_rows_basic.pyx"))
 
-# complilation: python load_attr_rows_new_setup.py build_ext --inplace
+# complilation: python load_attr_rows_basic_setup.py build_ext --inplace

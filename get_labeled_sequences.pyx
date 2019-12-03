e688a557837b6ba2519d7b5c3f44ccf445d33ddc
Jeff Coggshall <jeff.coggshall@macys.com>
2019-12-03 15:27:40 -0800

    automated commit from laptop Tue Dec  3 15:27:29 PST 2019

---
+++ get_labeled_sequences.pyx
@@ -0,0 +1,55 @@
+cimport cython
+
+from pandas import unique as puq
+from operator import itemgetter
+
+def unique_by_indices(list_of_stuff, indices=[0,-1]):
+    
+    cdef set seen
+    cdef list result
+
+    seen = set()
+    result = []
+    
+    for item in list_of_stuff:
+        set_matcher = tuple([item[x] for x in indices])
+        if set_matcher not in seen:
+            result.append(item)
+            seen.add(set_matcher)
+    return result
+
+def get_labeled_sequences(view_dict, atb_dict, purchase_dict, unique=True):
+    cdef list vs
+    cdef list vsl
+    cdef list at
+    cdef list atl
+    cdef list pu
+    cdef list pul
+    cdef list all_user_ids
+
+    atbids = list(atb_dict.keys())
+    puids = list(purchase_dict.keys())
+
+    all_user_ids = puq(atbids + puids)
+
+    for uid in all_user_ids:
+
+        if view_dict:
+            vs = sorted(view_dict[user_id],key=itemgetter(1))
+            vsl = [x + ("VIEW",) for x in vs]
+            if unique: vsl = unique_by_indices(vsl)
+        
+        at = sorted(atb_dict[user_id],key=itemgetter(1))
+        atl = [x + ("ATB",) for x in at]
+        if unique: atl = unique_by_indices(atl)
+        
+        pu = sorted(purchase_dict[user_id],key=itemgetter(1))
+        pul = [x + ("PURCHASE",) for x in pu]
+        if unique: pul = unique_by_indices(pul)
+    
+    if view_dict:
+        full_sequence = sorted(vsl+atl+pul,key=itemgetter(1))
+    else:
+        full_sequence = sorted(atl+pul,key=itemgetter(1))
+
+    return puq(full_sequence)

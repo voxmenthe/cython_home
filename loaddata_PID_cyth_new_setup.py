501c48a5dd4e0abbe2b2ae98da1522c657c3c391
Jeff Coggshall <jeff.coggshall@macys.com>
2019-12-30 09:55:39 -0800

    automated commit from laptop Mon Dec 30 09:55:20 PST 2019

--- loaddata_PID_cyth_new_setup.py
+++ loaddata_PID_cyth_new_setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 setup(ext_modules=cythonize("loaddata_PID_cyth_new.pyx"))
 
-# complilation: python csv_event_rows_to_dict_setup.py build_ext --inplace
+# complilation: python loaddata_PID_cyth_new_setup.py build_ext --inplace

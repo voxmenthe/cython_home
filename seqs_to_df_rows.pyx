fda101313a46e401e8486105749f7dffec4a62ed
Jeff Coggshall <jeff.coggshall@macys.com>
2020-01-06 13:12:27 -0800

    stdr tabs

--- seqs_to_df_rows.pyx
+++ seqs_to_df_rows.pyx
@@ -13,18 +13,18 @@ def seqs_to_df_rows(seqs, vector_dict, mmm_dict, N_ROWS=None):
 	
 	for actions in seqs[1:N_ROWS]:
 
-	    uid = actions[0]
+		uid = actions[0]
 
-	    for action in actions[1:]:
+		for action in actions[1:]:
 
-	        item_id = action[0]
-	        event_type = action[2]
+			item_id = action[0]
+			event_type = action[2]
 
-	        try:
-	            vector_dict[int(item_id)]
-	            output.append([uid, mmm_dict[uid], item_id, event_type] + vector_dict[int(item_id)])
+			try:
+				vector_dict[int(item_id)]
+				output.append([uid, mmm_dict[uid], item_id, event_type] + vector_dict[int(item_id)])
 
-	        except:
-	            continue
+			except:
+				continue
 
 	return output

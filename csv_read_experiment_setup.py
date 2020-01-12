40c48d6a631aceb15c59d5ae51dc64b8cbaaad8d
Jeff Coggshall <jeff.coggshall@macys.com>
2020-01-11 22:35:30 -0800

    automated commit from laptop Sat Jan 11 22:35:24 PST 2020

--- csv_read_experiment_setup.py
+++ csv_read_experiment_setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize("csv_read_experiment*.pyx"))
+import numpy
+
+setup(ext_modules=cythonize("csv_read_experiment*.pyx"),
+	include_dirs=[numpy.get_include()])
 
 # complilation: python csv_read_experiment_setup.py build_ext --inplace

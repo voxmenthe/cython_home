ec1dfd490b641e1b9b0d58a72ffa96dc14784be9
Jeff Coggshall <jeff.coggshall@macys.com>
2020-01-11 22:47:20 -0800

    automated commit from laptop Sat Jan 11 22:47:13 PST 2020

--- csv_to_dict_flex_new_setup.py
+++ csv_to_dict_flex_new_setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize("csv_to_dict_flex_new.pyx"))
+setup(ext_modules=cythonize("csv_to_dict_flex_new*.pyx"))
 
 # complilation: python csv_to_dict_flex_new_setup.py build_ext --inplace

e875dade444bdde68d7b824ec6fd9087f06f1e0a
Jeff Coggshall <jeff.coggshall@macys.com>
2020-01-11 23:44:47 -0800

    automated commit from laptop Sat Jan 11 23:44:40 PST 2020

--- csv_to_dict_flex_new_test.pyx
+++ csv_to_dict_flex_new_test.pyx
@@ -62,10 +62,64 @@ def csv_to_dict_flex_new_v1(filename,by="user",dedup=False):
  
     return output
 
-# @cython.boundscheck(False)
-# @cython.wraparound(False)
 
 def csv_to_dict_flex_new_v2(filename,by="user",dedup=False):
+    filename_byte_string = filename.encode("UTF-8")
+    cdef char* fname = filename_byte_string
+ 
+    cdef FILE* cfile
+    
+    cdef str decoded
+    cdef list row
+    cdef dict output
+    output = {}
+
+    cfile = fopen(fname, "rb")
+    if cfile == NULL:
+        raise FileNotFoundError(2, "No such file or directory: '%s'" % filename)
+
+    cdef char * line = NULL
+    cdef size_t l = 0
+    cdef ssize_t read
+
+    cdef str key
+    cdef str item_id
+    cdef str timestamp
+ 
+    while True:
+        read = getline(&line, &l, cfile)
+        if read == -1: break
+        decoded = line.decode("UTF-8")
+        row = decoded.split('\t')
+
+        if by == "user":
+            key = row[0]
+        elif by == "session":
+            key = row[3]
+        else:
+            print("Must organize by either user or session")
+
+        item_id = row[1]
+        timestamp = row[2]
+        #entry = (row[1],row[2])
+        entry = (item_id,timestamp)
+
+        if key in output.keys():
+            if dedup:
+                if entry not in output[key]:
+                    output[key].append(entry)                    
+            else:
+                output[key].append(entry)
+        else:
+            output[key] = [entry]
+ 
+    fclose(cfile)
+ 
+    return output
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def csv_to_dict_flex_new_v3(filename,by="user",dedup=False):
     filename_byte_string = filename.encode("UTF-8")
     cdef char* fname = filename_byte_string
  

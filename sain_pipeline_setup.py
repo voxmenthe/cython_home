ba6299a16c4838643e0e0766fa78865576c1348a
Jeff Coggshall <jeff.coggshall@macys.com>
2020-01-16 10:35:12 -0800

    automated commit at Thu Jan 16 10:35:12 PST 2020 from /Users/a091569/Desktop/OneDrive/macysrepos/cython_home

--- sain_pipeline_setup.py
+++ sain_pipeline_setup.py
@@ -1,5 +1,8 @@
+import numpy
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize("sain_pipeline.pyx"))
+
+setup(ext_modules=cythonize("sain_pipeline.pyx"),
+	include_dirs=[numpy.get_include()])
 
 # complilation: python sain_pipeline_setup.py build_ext --inplace
